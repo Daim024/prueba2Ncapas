e2a4b7ecffaf11587a2fe97da73815b55edbf5ce
David00047920 <00047920@uca.edu.sv>
2023-05-26 17:58:18 -0600

    modificando app.jsx

---
+++ initProyect/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useState } from 'react';
+import Login from '../src/components/Login.jsx'
+
+
+function App() {
+    return ( 
+
+        <div>
+
+            <Login />
+
+
+        </div>
+
+   
+
+
+    )  
+
+}
+export default App;
--- initProyect/src/components/Login.jsx
+++ initProyect/src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../src/assets/img/logo.png';
+import logo from '../assets/img/logo.png'
 import { useState } from 'react'
 import { useEffect } from 'react'
 import GoogleLogin from 'react-google-login'
